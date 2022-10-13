bf832eef2774be486be6f084a1a3f7ca28f87cfc
Árpás Péter <arpaspeter0@gmail.com>
2022-10-13 08:29:43 +0200

    Bankiszolgáltatás és tulajdonos osztály elkészítve

---
+++ src/hu/petrik/Orai1004/BankiSzolgaltatas.java
@@ -0,0 +1,14 @@
+package hu.petrik.Orai1004;
+
+public class BankiSzolgaltatas {
+
+    private String Tulajdonos;
+
+    public BankiSzolgaltatas(String tulajdonos) {
+        Tulajdonos = tulajdonos;
+    }
+
+    public String getTulajdonos() {
+        return Tulajdonos;
+    }
+}
---
+++ src/hu/petrik/Orai1004/Tulajdonos.java
@@ -0,0 +1,18 @@
+package hu.petrik.Orai1004;
+
+public final class Tulajdonos{
+
+    public String nev;
+
+    public Tulajdonos(String nev) {
+        this.nev = nev;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+}
